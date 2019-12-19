936af9e7b026eb687c508001c230b6c96bd9f1bb
John McKie <john.mckie@remarkable.net>
2019-12-19 15:53:20 +0000

    Chat app completed

--- ChatroomV2/scripts/ui.js
+++ ChatroomV2/scripts/ui.js
@@ -5,6 +5,10 @@ class ChatUI {
     constructor(list) {
         this.list = list;
     }
+    clear() {
+        this.list.innerHTML = '';
+
+    }
     render(data) {
         const when = dateFns.distanceInWordsToNow(
             data.created_at.toDate(),
@@ -12,8 +16,8 @@ class ChatUI {
         )
         const html = `
             <li class="list-group-item">
-                <span class="username">${data.username}<span>
-                <span class="message">${data.message}<span>
+                <span class="username">${data.username}</span>
+                <span class="message">${data.message}</span>
                 <div class="time">${when}</div>
             </li>
         `;
